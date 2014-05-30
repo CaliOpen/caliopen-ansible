bb21bec1bac927b6cc78e0e076dcd30cdc86aac1
Aymeric Barantal <mric@gandi.net>
2014-05-30 16:32:31 +0200

    an openvpn role

---
+++ roles/vpn/templates/bridge-start.sh.j2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#################################
+# Set up Ethernet bridge on Linux
+# Requires: bridge-utils
+#################################
+
+# Define Bridge Interface
+br="br0"
+
+# Define list of TAP interfaces to be bridged,
+# for example tap="tap0 tap1 tap2".
+tap="tap0"
+
+# Define physical ethernet interface to be bridged
+# with TAP interface(s) above.
+eth="eth1"
+eth_ip="{{ facter_network_eth1 }}"
+eth_netmask="{{ facter_netmask_eth1 }}"
+
+# XXX how to get this value ?
+eth_broadcast="10.0.0.255"
+
+for t in $tap; do
+    openvpn --mktun --dev $t
+done
+
+brctl addbr $br
+brctl addif $br $eth
+
+for t in $tap; do
+    brctl addif $br $t
+done
+
+for t in $tap; do
+    ifconfig $t 0.0.0.0 promisc up
+done
+
+ifconfig $eth 0.0.0.0 promisc up
+
+ifconfig $br $eth_ip netmask $eth_netmask broadcast $eth_broadcast
